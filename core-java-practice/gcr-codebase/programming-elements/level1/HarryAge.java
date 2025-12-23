8eb211fb4ab8750b157acd1b9d8baee787ef2a4b
Prince Kumar <princekumar07014@gmail.com>
2025-12-23 21:42:09 +0530

    fixing issues

--- core-java-practice/gcr-codebase/programming-elements/level1/HarryAge.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class HarryAge {
-    public static void main(String[] args) {
-        int dob = 2000;
-        int age = 2024 - dob;
-        System.out.println("Harry's age in 2024 is " + age);
-    }
-}
--- core-java-practice/gcr-codebase/programming-elements/level1/KmToMiles.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class KmToMiles {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double km = 10.8;
-        double mi = km * 0.621371;
-        System.out.println("The distance  "+km+" km in miles is "+mi);
-    }
-}
