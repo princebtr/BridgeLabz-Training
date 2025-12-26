78ca1018478c4b8308d6d54c9f61a2ade322863f
Prince Kumar <princekumar07014@gmail.com>
2025-12-26 15:57:14 +0530

    [Prince Kumar] Refactor . Refactored reviews

--- core-java-practice/gcr-codebase/programming-elements/basic/AreaOfCircle.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class AreaOfCircle {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double r = sc.nextDouble();
-        double area = 3.14 * r * r;
-        System.out.println(area);
-    }
-}
