91c9db8b70da4638c6928acdf7b2fd6ed8509ff7
Prince Kumar <princekumar07014@gmail.com>
2025-12-30 15:46:16 +0530

    [PrinceKumar] Refactor . refactored Snake and Ladder Game till UC7

--- core-java-practice/scenerio-based/SnakeAndLadder.java
+++ core-java-practice/scenerio-based/SnakeAndLadder.java
@@ -25,38 +25,60 @@ public class SnakeAndLadder {
 	}
 	
 //	use case 5
-	public static boolean winCheck(int points) {
+	public static boolean winCheck(int points, int player) {
 		if(points == 100) {
-			System.out.println("Player Wins!!!");
+			System.out.println("Player "+player+" Wins!!!");
 			return true;
 		}
 		return false;
+	}
+	
+//	use case 7
+	public static int rounds(int points) {
+//		use case 2
+		int numOnDice = rollDice();
+		System.out.println("Number you got is " + numOnDice);
+		
+//		use case 3
+		points = checkSum(points,numOnDice);
+		int[] arr = snakeOrLadder();
+		if(arr[0] == 1) {
+			System.out.println("You got NO PLAY");
+		} else if(arr[0] == 2) {
+			System.out.println("Yayy you got LADDER...move ahead "+arr[1]+" steps");
+		} else {
+			System.out.println("Oops you got SNAKE...move behind "+arr[1]+" steps");
+		}
+		return points;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game...");
 		
 //		use case 1
-		int points = 0;
+		int points1 = 0;
+		int points2 = 0;
+		int count = 0;
 		boolean win = false;
 		
+//		use case 7
 		while(!win) {
-//			use case 2
-			int numOnDice = rollDice();
-			System.out.println("Number you got is " + numOnDice);
+//			player 1 turn
+			System.out.println("PLAYER 1 TURN");
+			points1 = rounds(points1);
+			win = winCheck(points1,1);
+			System.out.println("Points of Player 1: "+points1);
+
 			
-//			use case 3
-			points = checkSum(points,numOnDice);
-			int[] arr = snakeOrLadder();
-			if(arr[0] == 1) {
-				System.out.println("You got NO PLAY");
-			} else if(arr[0] == 2) {
-				System.out.println("Yayy you got LADDER...move ahead "+arr[1]+" steps");
-			} else {
-				System.out.println("Oops you got SNAKE...move behind "+arr[1]+" steps");
-			}
-			win = winCheck(points);
+//			player 1 turn
+			System.out.println("PLAYER 2 TURN");
+			points2 = rounds(points2);
+			win = winCheck(points2,2);
+			System.out.println("Points of Player 2: "+points2);
+			count++;
 		}
+		
+		System.out.println("Dice was played "+count+" times");
 
 	}
 
