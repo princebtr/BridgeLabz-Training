2b873ad210eb86f7a5199bea5139742a04d1fc63
Prince Kumar <princekumar07014@gmail.com>
2026-01-10 21:56:33 +0530

    [Prince Kumar] Refactor . updated main folder structure

--- core-java-practice/gcr-codebase/programming-elements/level1/PenDistribution.java
+++
@@ -1,9 +0,0 @@
-public class PenDistribution {
-    public static void main(String[] args) {
-        int pens = 14;
-        int students = 3;
-        int perStudent = pens / students;
-        int rem = pens % students;
-        System.out.println("The Pen Per Student is "+perStudent+" and the remaining pen not distributed is "+rem);
-    }
-}
