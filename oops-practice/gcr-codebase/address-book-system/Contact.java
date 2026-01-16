b490a02e65afc7a9525279010af1cfea9ad01b75
Prince Kumar <princekumar07014@gmail.com>
2026-01-16 12:33:58 +0530

    [Prince Kumar] Add . added use case 1 of address book system

---
+++ oops-practice/gcr-codebase/address-book-system/Contact.java
@@ -0,0 +1,78 @@
+//Use Case 1
+//Ability to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email...
+package AddressBookSystem;
+
+public class Contact {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String email;
+	
+	public Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	
+	
+//	Getter
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public String getState() {
+		return state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	
+//	Setters
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
